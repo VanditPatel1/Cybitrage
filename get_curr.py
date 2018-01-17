c5b2f3f863cf499c41fa473b018a3f955d1b3afd
Jerry Guo <jerrguo@gmail.com>
2018-01-17 18:32:40 -0500

    added a currencies variable to api

--- get_curr.py
+++ get_curr.py
@@ -1,16 +1,23 @@
-import pprint
 import json
 import requests
 from API_KEYS import *
 
 BASE_URL = 'http://apilayer.net/api'
 
-#list of all curriences
-res = requests.get(BASE_URL + '/list' + API_TOKEN1)
-
-#print (str(json.loads(res.text)).encode("utf-8")) BELOW IS A SIMPLER WAY TO DO THIS
-print (str(res.json()).encode("utf-8"))
-
-
 ### '/list' returns all curriences supported
 LIST = '/list'
+
+#list of all curriences
+#res = requests.get(BASE_URL + LIST + API_TOKEN1)
+##print (str(json.loads(res.text)).encode("utf-8")) BELOW IS A SIMPLER WAY TO DO THIS
+#print (str(res.json()).encode("utf-8"))
+
+currencies = [] #stores a copy of the currencies which will be used (for the API)
+
+#copy the list of currencies into variable currencies
+def get_currencies(copy):
+    global currencies
+    currencies = copy
+
+#create the currency exchange values as directed edges of the graph and return as array or something
+def create_edges():
