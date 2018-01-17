ebcde97a1caf6f57e64ad99ee412387a3acd1422
Jerry Guo <jerrguo@gmail.com>
2018-01-16 19:51:04 -0500

    simplified printing of json request

--- get_curr.py
+++ get_curr.py
@@ -1,3 +1,4 @@
+import pprint
 import json
 import requests
 from API_KEYS import *
@@ -5,10 +6,10 @@ from API_KEYS import *
 BASE_URL = 'http://apilayer.net/api'
 
 #list of all curriences
-res = requests.get(BASE_URL+'/list' + API_TOKEN1)
+res = requests.get(BASE_URL + '/list' + API_TOKEN1)
 
-print (str(json.loads(res.text)).encode("utf-8"))
-
+#print (str(json.loads(res.text)).encode("utf-8")) BELOW IS A SIMPLER WAY TO DO THIS
+print (str(res.json()).encode("utf-8"))
 
 
 ### '/list' returns all curriences supported
