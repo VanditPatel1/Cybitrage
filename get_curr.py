a3d1f76174a24fe757f29a8580492f4e6364cc69
Vandit Patel <patel.vandit1@gmail.com>
2018-01-18 18:31:26 -0500

    Got new API working kinda

--- get_curr.py
+++ get_curr.py
@@ -2,22 +2,35 @@ import json
 import requests
 from API_KEYS import *
 
-BASE_URL = 'http://apilayer.net/api'
+BASE_URL = 'https://forex.1forge.com/1.0.2'
 LIVE = '/live' #for live results
 CURRENCIES = '&currencies='
+QUOTES = '/quotes?pairs=EURUSD,GBPJPY,AUDUSD'
 SOURCE = '&source='
 FORMAT = '&format=1'
 LIST = '/list' 
 LIST_CURR = ['CAD', 'JPY', 'INR', 'GBP', 'AUD', 'USD']
 
-print BASE_URL+LIVE+API_TOKEN1+CURRENCIES+','.join(LIST_CURR)
+#print BASE_URL+LIVE+API_TOKEN1+CURRENCIES+','.join(LIST_CURR)
 
-def USD_Exchanges():
-	rates = requests.get(BASE_URL+LIVE+API_TOKEN1+CURRENCIES+','.join(LIST_CURR)+SOURCE+'USD'+FORMAT)
-	print rates.text
+print 'https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD,GBPJPY,AUDUSD&api_key=fpD9v4fbs0LBz0D2bP6aox0cxhUF6maf'
 
-USD_Exchanges()
+print BASE_URL+QUOTES+API_TOKEN1
+#def USD_Exchanges():
+#	rates = requests.get(BASE_URL+LIVE+API_TOKEN1+CURRENCIES+','.join(LIST_CURR)+SOURCE+'USD'+FORMAT)
+#	print rates.text
 
+#USD_Exchanges()
+
+
+def get_all_exhange_rates():
+	for curr in LIST_CURR:
+		print BASE_URL+QUOTES+API_TOKEN1
+		rates = requests.get(BASE_URL+QUOTES+API_TOKEN1)
+		#rates = requests.get(BASE_URL+LIVE+API_TOKEN1+CURRENCIES+','.join(LIST_CURR)+SOURCE+str(curr)+FORMAT)
+		print rates
+
+get_all_exhange_rates()	
 
 #list of all curriences
 #res = requests.get(BASE_URL + LIST + API_TOKEN1)
