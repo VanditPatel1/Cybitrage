49439b8f05d8edcf14ced1cc7e7180c2c4d5f80e
Vandit Patel <patel.vandit1@gmail.com>
2018-01-19 12:10:06 -0500

    check out some of the comments

--- weighted_graph.py
+++ weighted_graph.py
@@ -1,8 +1,13 @@
 import math
 from get_curr import *
 
+###############################
+##Lets make this into a class##
+###I think it will be useful###
+###############################
+
 #Look-Up Table for Edge Weights
-edges = get_data()
+edges = get_data() #why is this a global?
 
 def create_shortest_path_table():
 
@@ -13,8 +18,9 @@ def create_shortest_path_table():
     return distances
 
 def get_weight(currency):
-
-    global edges
+    #global edges
+    edges = get_data() #we can get the frame here
+	
     return edges.get_value(currency, 'ask') #returns edge weights based on 'ask' price
 
 
