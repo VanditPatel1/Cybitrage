e105cf486c51e232471a64313d653e977fd00095
Vandit Patel <patel.vandit1@gmail.com>
2018-01-18 23:21:19 -0500

    updated get_curr.py

--- get_curr.py
+++ get_curr.py
@@ -1,34 +1,41 @@
 import json
 import requests
 import itertools
+import pandas as pd
 from API_KEYS import *
 
 BASE_URL = 'https://forex.1forge.com/1.0.2'
 QUOTES = '/quotes?pairs='
-CURRENCIES = ['USD', 'CAD', 'GBP', 'JPY', 'AUD']
+CURRENCIES = ['USD', 'CAD', 'GBP', 'JPY', 'INR']
 SOURCE = '&source='
-
-def get_curr_combos(currList):
-
-	all_combos = []
-	global CURRENCIES
-	mix_curr = list(itertools.product(CURRENCIES, CURRENCIES)) #cross all the curriences
-	for d_curr in mix_curr:
-		if not d_curr[0] == d_curr[1]: #only join diffrent curriences, ignore the same ones
-			all_combos.append(''.join(d_curr))
-
-	return all_combos
 
 def get_all_exhange_rates(all_combos):
 
-	all_combos = ','.join(all_combos) #join all currencies in list
-	rates = requests.get(BASE_URL + QUOTES + all_combos + API_TOKEN1) #get rates from API
-	exhange_rates = json.loads(rates.content) #load into json format
-	all_info = json.dumps(exhange_rates, indent=4) #get visuals for print
+        all_combos = ','.join(all_combos) #join all currencies in list
+        rates = requests.get(BASE_URL+QUOTES+all_combos+API_TOKEN1) #get rates from API
+        all_info = json.loads(rates.content) #load into json format
+        #all_info = json.dumps(exhange_rates, indent=4) #get visuals for print
 
-	return all_info, all_combos
+        return all_info, all_combos
 
-combos = get_curr_combos(CURRENCIES)
+
+def mix_curr(currList):
+
+        all_combos = []
+        mix_curr = list(itertools.product(currList, currList)) #cross all the curriences
+        for d_curr in mix_curr:
+                if not d_curr[0] == d_curr[1]: #only join diffrent curriences, ignore the same ones
+                        all_combos.append(''.join(d_curr))
+
+        return all_combos
+
+
+def place_in_dict(all_info, all_combos):
+        df = pd.DataFrame(all_info)
+        print df
+
+
+
+combos = mix_curr(CURRENCIES)
 a, b = get_all_exhange_rates(combos)
-
-print (a)
+place_in_dict(a, b)
