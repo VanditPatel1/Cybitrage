089236d025cdc9a836a752689f93db8aaac84499
Vandit Patel <patel.vandit1@gmail.com>
2018-01-18 21:54:22 -0500

    got all exhange rates returning in JSON

--- get_curr.py
+++ get_curr.py
@@ -1,48 +1,37 @@
 import json
 import requests
+import itertools
 from API_KEYS import *
 
 BASE_URL = 'https://forex.1forge.com/1.0.2'
-LIVE = '/live' #for live results
-CURRENCIES = '&currencies='
-QUOTES = '/quotes?pairs=EURUSD,GBPJPY,AUDUSD'
+QUOTES = '/quotes?pairs='
+CURRENCIES = ['USD', 'CAD', 'GBP', 'JPY', 'INR']
 SOURCE = '&source='
-FORMAT = '&format=1'
-LIST = '/list' 
-LIST_CURR = ['CAD', 'JPY', 'INR', 'GBP', 'AUD', 'USD']
 
-#print BASE_URL+LIVE+API_TOKEN1+CURRENCIES+','.join(LIST_CURR)
+def get_all_exhange_rates(all_combos):
 
-print 'https://forex.1forge.com/1.0.2/quotes?pairs=EURUSD,GBPJPY,AUDUSD&api_key=fpD9v4fbs0LBz0D2bP6aox0cxhUF6maf'
+	all_combos = ','.join(all_combos) #join all currencies in list
+	rates = requests.get(BASE_URL+QUOTES+all_combos+API_TOKEN1) #get rates from API 
+	exhange_rates = json.loads(rates.content) #load into json format
+	all_info = json.dumps(exhange_rates, indent=4) #get visuals for print
 
-print BASE_URL+QUOTES+API_TOKEN1
-#def USD_Exchanges():
-#	rates = requests.get(BASE_URL+LIVE+API_TOKEN1+CURRENCIES+','.join(LIST_CURR)+SOURCE+'USD'+FORMAT)
-#	print rates.text
-
-#USD_Exchanges()
+	return all_info, all_combos
 
 
-def get_all_exhange_rates():
-	for curr in LIST_CURR:
-		print BASE_URL+QUOTES+API_TOKEN1
-		rates = requests.get(BASE_URL+QUOTES+API_TOKEN1)
-		#rates = requests.get(BASE_URL+LIVE+API_TOKEN1+CURRENCIES+','.join(LIST_CURR)+SOURCE+str(curr)+FORMAT)
-		print rates
+def mix_curr(currList):
 
-get_all_exhange_rates()	
+	all_combos = []
+	mix_curr = list(itertools.product(currList, currList)) #cross all the curriences
+	for d_curr in mix_curr: 
+		if not d_curr[0] == d_curr[1]: #only join diffrent curriences, ignore the same ones
+			all_combos.append(''.join(d_curr))
+	
+	return all_combos
 
-#list of all curriences
-#res = requests.get(BASE_URL + LIST + API_TOKEN1)
-##print (str(json.loads(res.text)).encode("utf-8")) BELOW IS A SIMPLER WAY TO DO THIS
-#print (str(res.json()).encode("utf-8"))
+combos = mix_curr(CURRENCIES)
+a, b = get_all_exhange_rates(combos)
 
-currencies = [] #stores a copy of the currencies which will be used (for the API)
+print a
+		
 
-#copy the list of currencies into variable currencies
-#def get_currencies(copy):
-#    global currencies
-#    currencies = copy
 
-#create the currency exchange values as directed edges of the graph and return as array or something
-#def create_edges():
