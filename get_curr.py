e539a638051e3da5871e08da71fa4af54ada6e33
Jerry Guo <jerrguo@gmail.com>
2018-01-20 00:28:47 -0500

    lmao buggy bellman ford i think

--- get_curr.py
+++ get_curr.py
@@ -10,30 +10,36 @@ CURRENCIES = ['USD', 'CAD', 'GBP', 'JPY', 'AUD']
 SOURCE = '&source='
 
 def get_num_currencies(currs):
-    currs = CURRENCIES # TODO replace this later when inputs are a curriencies list
+    currs = CURRENCIES                                      # TODO replace this later when inputs are a curriencies list
     return len(currs)
 
 def get_curr_combos(combos):
-	combos = CURRENCIES # TODO replace this later when inputs are a curriencies list
+	combos = CURRENCIES                                     # TODO replace this later when inputs are a curriencies list
 	all_combos = []
-	mix_curr = list(itertools.product(combos, combos))                          #cross all the curriences
+	mix_curr = list(itertools.product(combos, combos))      # cross all the curriences
 	for d_curr in mix_curr:
-		if not d_curr[0] == d_curr[1]:                                          #only join diffrent curriences, ignore the same ones
+		if not d_curr[0] == d_curr[1]:                      # only join different curriences, ignore the same ones
 			all_combos.append(''.join(d_curr))
 
 	return all_combos
 
 def get_all_exhange_rates(all_combos):
 
-        all_combos = ','.join(all_combos)                                       #join all currencies in list
-        rates = requests.get(BASE_URL + QUOTES + all_combos + API_TOKEN1)       #get rates from API
-        all_info = json.loads(rates.content)                                    #load into json format
-        #all_info = json.dumps(exhange_rates, indent=4) #get visuals for print
+        all_combos = ','.join(all_combos)                                       # join all currencies in list
+        rates = requests.get(BASE_URL + QUOTES + all_combos + API_TOKEN1)       # get rates from API
+        all_info = json.loads(rates.content)                                    # load into json format
+        #all_info = json.dumps(exhange_rates, indent=4)                         # get visuals for print
 
         return all_info, all_combos
 
-def place_in_dict(all_info, all_combos):
+def place_in_dict(all_info, all_combos, currs):
         df = pd.DataFrame(all_info)
+
+        for currency in currs:
+            d = {'ask': [1], 'bid': [1], 'price': [1], 'symbol':['STA'+currency], 'timestamp': [0]}     # Add START (STA) nodes in the graph for edge look-up
+            df2 = pd.DataFrame(data=d)
+            df = df.append(df2, ignore_index=True)
+
         df = df.sort_values(by=['symbol'])
         df = df.reset_index(drop=True)
         df = df.set_index('symbol')
@@ -42,5 +48,5 @@ def place_in_dict(all_info, all_combos):
 def get_data(currs):
 	combos = get_curr_combos(currs)
 	a, b = get_all_exhange_rates(combos)
-	df = place_in_dict(a, b)
+	df = place_in_dict(a, b, currs)
 	return df
--- weighted_graph.py
+++ weighted_graph.py
@@ -6,16 +6,27 @@ from get_curr import *
 ##  I think it will be useful  ##
 #################################
 
-def create_shortest_path_table(curr):
+def create_distance_table(curr):
 
-    global CURRENCIES # TODO replace this later when inputs are a curriencies list
-    curr = CURRENCIES # TODO replace this later when inputs are a curriencies list
-    distances = {"start" : 0}
+    global CURRENCIES                                       # TODO replace this later when inputs are a curriencies list
+    curr = CURRENCIES                                       # TODO replace this later when inputs are a curriencies list
+    distances = {"STA" : 0}                                 # STA stands for START
     for currency in curr:
-        distances[currency] = float("inf")                  #set all starting vertices to be infinite distance away
-    for x in distances:
-        print (x, distances[x])
+        distances[currency] = 0                             # set all starting vertices to be infinite distance away
+    # for x in distances:
+    #     print (x, distances[x])
     return distances
+
+def create_predecessor_table(curr):
+
+    global CURRENCIES                                       # TODO replace this later when inputs are a curriencies list
+    curr = CURRENCIES                                       # TODO replace this later when inputs are a curriencies list
+    predecessor = {}
+    for currency in curr:
+        predecessor[currency] = None                        #set all starting vertices to have no predecessor
+    # for x in predecessor:
+    #     print (x, predecessor[x])
+    return predecessor
 
 ###############################        BELLMAN-FORD ALGORITHM      ###############################
 
@@ -24,19 +35,84 @@ class weighted_graph:
     def __init__(self, currs):
 
         self.currencies = currs                             # list of currencies
-        self.curr_df = get_data(currs)                      # currency data frame
-        #print (self.curr_df)
-        self.sp_table = create_shortest_path_table(currs)   # shortest path table
+        self.curr_df = get_data(currs)                      # currency data frame (with listings of all edges)
+        print (self.curr_df)
+        self.dist_table = create_distance_table(currs)      # shortest path table
+        self.pre_table = create_predecessor_table(currs)    # predecessor table
 
     def get_weight(self, edge):
 
         edge_rate = self.curr_df.get_value(edge, 'ask')     # edge weights based on 'ask' price
         edge_log_rate = (-1)*math.log(edge_rate, 2.0)       # MUST CONVERT TO LOG-EXCHANGE RATE FOR BELLMAN-FORD
         #print (edge_log_rate)
-        return self.curr_df.get_value(edge, 'ask')
+        return edge_log_rate
+
+    def has_arbitrage_opportunity(self):
+
+        print ('STARTING BELLMAN FORD')
+        updated = True                                                  # keeps track of whether an update was made or not
+        iteration = 1                                                   # keeps track of the iteration number
+        num_currencies = get_num_currencies(self.currencies)            # number of currencies
+        currency_list = ['STA'] + self.currencies                       # temp list to store currencies + STA node
+        print ('NUM CURR: ' + str(num_currencies))
+
+        while iteration < num_currencies and updated == True:
+            updated = False                                             # reset updated for checking if an update occurs later
+            print ('LOOP ACCESSED')
+            print (self.currencies)
+            for from_curr in currency_list:                             # First Loop for each currency
+                for to_curr in currency_list:                           # Second Loop for each currency that...
+                    if (to_curr != from_curr and to_curr != 'STA'):     # ... is a different currency than the first (and is not STA)
+
+                        print ('CURRENCY DETECTION: ' + from_curr + to_curr)
+                        print (str(self.dist_table[from_curr] + self.get_weight(from_curr + to_curr)))
+                        if self.dist_table[from_curr] + self.get_weight(from_curr + to_curr) < self.dist_table[to_curr]:         # if a shorter path is found to THIS node
+                            self.dist_table[to_curr] = self.dist_table[from_curr] + self.get_weight(from_curr + to_curr)         # update distance table with shorter path distance
+                            self.pre_table[to_curr] = from_curr                                                                  # update predecessor table with shorter path node
+                            updated = True                                                                                       # an update occurred so updated = True
+
+            iteration = iteration + 1
+
+
+        if updated == True:                                             # FINAL ITERATION to detect NEGATIVE WEIGHT CYCLES
+
+            for from_curr in self.currencies:                           # First Loop for each currency
+                for to_curr in self.currencies:                         # Second Loop for each currency that...
+                    if (to_curr != from_curr and to_curr != 'STA'):     # ... is a different currency than the first (and is not STA)
+
+                        if self.dist_table[from_curr] + self.get_weight(from_curr + to_curr) < self.dist_table[to_curr]:    # negative cycle detected
+                            self.dist_table[to_curr] = float("-inf")                                                        # update distance table with -infinity
+                            self.pre_table[to_curr] = from_curr                                                             # update predecessor table with shorter path node
+                            return True
+
+        return False
+
+    def show_arbitrage_opportunity(self):
+
+        arbitrage_currencies = self.currencies                      # temp list used to display currencies with arbitrage opportunities
+
+        if self.has_arbitrage_opportunity():
+
+            for currency in arbitrage_currencies:
+                if self.dist_table[currency] != float("-inf"):      # no arbitrage opportunity detected
+                    arbitrage_currencies.remove(currency)           # remove currency from list of currencies with arbitrage opportunities
+
+            # for currency in arbitrage_currencies:
+            #     predecessor = pre_table[currency]                 # first predecessor in arbitrage cycle
+            #     while predecessor != currency:                    # while the cycle hasn't looped to beginning
+
+        for i in arbitrage_currencies:
+            print('----------------------')
+            print (i)
+        return (arbitrage_currencies)
 
 
 
 
-test = weighted_graph(['test'])
+test = weighted_graph(['USD', 'CAD', 'GBP', 'JPY', 'AUD'])
+test.show_arbitrage_opportunity()
+print ('+++++++++++++++++++++++++++')               # FOR TESTING
+print (test.dist_table)                             # FOR TESTING
+print ('**************************')                # FOR TESTING
+print (test.pre_table)                              # FOR TESTING
 test.get_weight('USDJPY')
