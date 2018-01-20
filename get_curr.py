fd8f11189e8789440aa54a23bb42fe20d109c7aa
Jerry Guo <jerrguo@gmail.com>
2018-01-19 21:12:47 -0500

    updated weightings to log exchange rate and other minor shit

--- get_curr.py
+++ get_curr.py
@@ -10,24 +10,24 @@ CURRENCIES = ['USD', 'CAD', 'GBP', 'JPY', 'AUD']
 SOURCE = '&source='
 
 def get_num_currencies(currs):
-    currs = CURRENCIES
+    currs = CURRENCIES # TODO replace this later when inputs are a curriencies list
     return len(currs)
 
 def get_curr_combos(combos):
-	combos = CURRENCIES #replace this later when inputs are a curriencies list
+	combos = CURRENCIES # TODO replace this later when inputs are a curriencies list
 	all_combos = []
-	mix_curr = list(itertools.product(combos, combos)) #cross all the curriences
+	mix_curr = list(itertools.product(combos, combos))                          #cross all the curriences
 	for d_curr in mix_curr:
-		if not d_curr[0] == d_curr[1]: #only join diffrent curriences, ignore the same ones
+		if not d_curr[0] == d_curr[1]:                                          #only join diffrent curriences, ignore the same ones
 			all_combos.append(''.join(d_curr))
-	
+
 	return all_combos
 
 def get_all_exhange_rates(all_combos):
 
-        all_combos = ','.join(all_combos) #join all currencies in list
-        rates = requests.get(BASE_URL + QUOTES + all_combos + API_TOKEN1) #get rates from API
-        all_info = json.loads(rates.content) #load into json format
+        all_combos = ','.join(all_combos)                                       #join all currencies in list
+        rates = requests.get(BASE_URL + QUOTES + all_combos + API_TOKEN1)       #get rates from API
+        all_info = json.loads(rates.content)                                    #load into json format
         #all_info = json.dumps(exhange_rates, indent=4) #get visuals for print
 
         return all_info, all_combos
--- weighted_graph.py
+++ weighted_graph.py
@@ -1,38 +1,42 @@
 import math
 from get_curr import *
 
-###############################
-##Lets make this into a class##
-###I think it will be useful###
-###############################
+#################################
+## Lets make this into a class ##
+##  I think it will be useful  ##
+#################################
 
-#Look-Up Table for Edge Weights
-#edges = get_data() #why is this a global?
+def create_shortest_path_table(curr):
 
-def create_shortest_path_table():
-
-    global CURRENCIES
-    distances = {}
-    for curr in list(edges.index.values):
-        distances[curr] = float("inf") #set all starting vertices to be infinite distance away
+    global CURRENCIES # TODO replace this later when inputs are a curriencies list
+    curr = CURRENCIES # TODO replace this later when inputs are a curriencies list
+    distances = {"start" : 0}
+    for currency in curr:
+        distances[currency] = float("inf")                  #set all starting vertices to be infinite distance away
+    for x in distances:
+        print (x, distances[x])
     return distances
 
-def get_weight(currency):
-    #global edges
-    #edges = get_data() #we can get the frame here
-	
-    return edges.get_value(currency, 'ask') #returns edge weights based on 'ask' price
-
-#BELLMAN-FORD ALGORITHM
-#create_shortest_path_table()
-
-
-
+###############################        BELLMAN-FORD ALGORITHM      ###############################
 
 class weighted_graph:
-	def __init__(self, currs):
-		curr_df = get_data(currs)
-		print curr_df
+
+    def __init__(self, currs):
+
+        self.currencies = currs                             # list of currencies
+        self.curr_df = get_data(currs)                      # currency data frame
+        #print (self.curr_df)
+        self.sp_table = create_shortest_path_table(currs)   # shortest path table
+
+    def get_weight(self, edge):
+
+        edge_rate = self.curr_df.get_value(edge, 'ask')     # edge weights based on 'ask' price
+        edge_log_rate = (-1)*math.log(edge_rate, 2.0)       # MUST CONVERT TO LOG-EXCHANGE RATE FOR BELLMAN-FORD
+        #print (edge_log_rate)
+        return self.curr_df.get_value(edge, 'ask')
 
 
-test = weighted_graph('test')	
+
+
+test = weighted_graph(['test'])
+test.get_weight('USDJPY')
