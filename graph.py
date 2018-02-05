47b5d8ab6fabfd15b26ec5a48fe5e3404cab7a24
VanditPatel1 <patel.vandit1@gmail.com>
2018-02-04 23:26:45 -0500

    new graph file

--- graph.py
+++ graph.py
@@ -1,12 +1,15 @@
 import math
 from currs.get_curr_new import *
 
+
 class weighted_graph:
 
     def __init__(self, currs):
 
         self.currencies = currs                                         # list of currencies
         self.curr_df, self.curr_matrix = get_data(currs)                # currency data frame (with listings of all edges)
+
+        print (self.currencies)
 
     def dist_pred_dict(self, curr):
         """
@@ -16,26 +19,42 @@ class weighted_graph:
         set source curr, in varible 'curr'
         to 0
         """
-
         dist = {}
         pred = {}
-        for currency in curr:
+        for currency in self.currencies:
             dist[currency] = float('inf')                  # set all starting vertices to be infinite distance away
             pred[currency] = None
 
         dist[curr] = 0
+
         return dist, pred
 
-    def get_val(self, row, column):
-        print self.curr_matrix.loc[self.currencies[row], self.currencies[column]]
+
+    def get_weight(self, row, column):
+
+        return self.curr_matrix.loc[row, column]
+
+
+    def relax(self, node, next, dist, pred):
+
+        if dist[next] > dist[node] + self.get_weight(node, next):
+            dist[next] = dist[node] + self.get_weight(node, next)
+            pred[next] = node 
 
     def find_arb_opp(self):
 
-        for curr in self.currencies:
-            
+        for to_curr in self.currencies:
+            dist, pred = self.dist_pred_dict(to_curr)
+
+            for x in range(0, len(self.currencies)-1):
+                for u in self.currencies:
+                    for v in self.currencies:
+                        relax(u, v, dist, pred)
+
 
 
 
 
 w = weighted_graph(['USD', 'CAD', 'GBP', 'JPY', 'AUD'])
-w.test()
+w.find_arb_opp()
+
