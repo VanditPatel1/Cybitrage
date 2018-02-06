543435598d9d11b34a406e4f98e9db4f69ea60f5
VanditPatel1 <patel.vandit1@gmail.com>
2018-02-06 14:50:45 -0500

    getting logic for table set up

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
 from currs.weighted_graph import weighted_graph
+
+
 app = Flask(__name__)
 
 
@@ -7,21 +9,45 @@ app = Flask(__name__)
 @app.route('/test', methods=['GET', 'POST'])
 def home():
 
-    return render_template('test.html')
+    currs=None
+
+    if request.method == 'POST':
+        c = request.form.getlist('currencies')
+        w = weighted_graph(c)
+        currs = w.curr_df
+        currs = currs.to_dict(orient='dict')
+        print currs
+
+    return render_template('test.html', currs=currs)
+
 
 
 @app.route('/currs', methods=['POST'])
 def currs():
-
     c = request.form.getlist('currencies')
     print c
     if len(c) > 1:
-        get_df(c)
+        currs = get_df(c)
+        session['currs'] = currs
+
+
     return redirect('/test')
 
+'''
+@app.route('/home', methods=['GET'])
+def home(currs):
+    currs = currs.to_dict(orient='dict')
+    return render_template('test.html', currs=currs)
+'''
+
 def get_df(data):
-    print data
+
     w = weighted_graph(data)
+    currs = w.curr_df
+    currs = currs.to_dict(orient='dict')
+    return (currs)
+
+
 
 
 #
